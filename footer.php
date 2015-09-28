061eb0375b67c7803d6d6a8744b55c200c7ec8a9
mnittoli <nittoli1111@gmail.com>
2015-09-28 10:42:55 -0700

    theme files update

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Nittoli
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nittoli' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'nittoli' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'nittoli' ), 'nittoli', '<a href="http://www.mikenittoli.com" rel="designer">Mike Nittoli</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
