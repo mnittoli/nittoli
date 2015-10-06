a753e737a2f76a4573303cc04e492f65c8e2f704
username at github <your email>
2015-10-05 21:46:23 -0700

    ContentPortfolio

---
+++ template-parts/content-portfolio.php
@@ -0,0 +1,9 @@
+<p>Content Loads</p>
+<?php $posts = get_posts( category_name => 'portfolio' ); ?>
+<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+	<p>It works</p>
+	<li>
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</li>
+<?php endforeach;
+wp_reset_postdata();?>
