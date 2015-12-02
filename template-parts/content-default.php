0fa574dd3a5dc9e0a1b87a0174c5e42de2d4060d
mnittoli <nittoli1111@gmail.com>
2015-12-02 09:28:42 -0800

    Index_Page_Template

--- template-parts/content-default.php
+++ template-parts/content-default.php
@@ -11,14 +11,22 @@
 
 <article id="post-<?php the_ID(); ?>" class="post-2col <?php post_class(); ?>"
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title left"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
+		<?php the_title( sprintf( '<h2 class="entry-title left"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			<div class="entry-meta left">
+				<span class="capital"><?php the_author(); ?></span>
+				<?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?>
+				<span><?php the_date(); ?>		</span>		
+			</div><!-- .entry-meta -->
+				<!-- Begin Featured Image -->
+			<?php $thumb_id = get_post_thumbnail_id();
+				$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+				$thumb_url = $thumb_url_array[0]; ?>
+			<div class='featured-image' style="background-image: url('<?php echo ($thumb_url); ?>');">
+			</div>
+		 <!-- End Featured Image -->
 		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta left">
-			<span class="capital"><?php the_author(); ?></span>
-			<?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?>
-			<span><?php the_date(); ?>		</span>		
-		</div><!-- .entry-meta -->
+		
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
