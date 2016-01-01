8be3edd7426d654c76176b57b2672ed64f97d846
mnittoli <nittoli1111@gmail.com>
2015-12-31 21:14:16 -0800

    Portfolio Plus Sign

--- category-portfolio.php
+++ category-portfolio.php
@@ -33,7 +33,8 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 			<div class="portfolio-content"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-				<div class="portfolio-title"><div class="title-txt"><?php the_title(); ?></div></div>
+				<div class="portfolio-title"><div class="title-txt"><span class="title-icon">
+				<img src="<?php echo get_template_directory_uri() . '/images/plussign5.png'; ?>"/></span><p><?php the_title(); ?></p></div></div>
 				<?php if (has_post_thumbnail() ){
 					the_post_thumbnail();
 				}
