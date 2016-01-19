130504e1a28cdb79aa69e94dd735420ca49c61aa
mnittoli <nittoli1111@gmail.com>
2016-01-19 08:24:45 -0800

    Isotope Issue

--- category-portfolio.php
+++ category-portfolio.php
@@ -6,7 +6,6 @@
  *
  * @package Nittoli
  */
-
 get_header(); ?>
 
 
@@ -30,19 +29,19 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<div class="sub-categories">
-				<span class="sub-categories category-all">All</span>
+				<span class="sub-categories category-all" onclick=showall()>All</span>
 				<?php $args = array('child_of' => 3);
 					$categories = get_categories( $args );
 					foreach($categories as $category) { 
-					    echo '<span  title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" class="sub-categories category-'.get_cat_id($category->name).' "' . '>' . $category->name.'</span> '; 
+					    echo '<span onclick=filteritman("'.$category->name.'") title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" class="sub-categories category-'.get_cat_id($category->name).' "' . '>' . $category->name.'</span> '; 
 					}
 				?>
 			</div>
 
-			<div id="portfolio-grid" data-columns><!-- Grid Begins -->
+			<div id="portfolio-grid"><!-- Grid Begins -->
 			<?php while ( have_posts() ) : the_post(); ?>
 		
-			<div class="portfolio-content grid-item
+			<div class="portfolio-content
 			<?php 
 			$posttags = get_post_class();
 			if ($posttags) {
@@ -50,7 +49,7 @@ get_header(); ?>
 					echo $tag.' ';
 				}
 			}
-			?>">
+			?>" style="width:25%">
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 
 
@@ -81,91 +80,16 @@ get_header(); ?>
 
 <?php //get_sidebar(); ?>
 <script>
-jQuery('.grid').isotope({
+$grid = jQuery('#portfolio-grid').isotope({
   // options
-  itemSelector: '.grid-item',
+  itemSelector: '.portfolio-content',
   layoutMode: 'fitRows'
 });
-
-/*var websiteDesign = true;
-var print = true;
-var mobileApps = true;
-var all = true;
-var websiteDesignDOM = jQuery(".category-website-design");
-var printDOM = jQuery(".category-print");
-var mobileAppsDOM = jQuery(".category-mobile-apps");
-var allDOM = jQuery(".category-website-design, .category-print, .category-mobile-apps");
-var websiteDesignTrigger=jQuery(".category-4");
-var mobileAppsTrigger=jQuery(".category-5");
-var printTrigger=jQuery(".category-6");
-var allTrigger=jQuery(".category-all");
-websiteDesignTrigger.click(function() {
-	websiteDesign = true;
-	print = false;
-	mobileApps = false;
-	all = false;
-	filterMaFolio();
-	console.log("websiteDesignTrigger");
-});
-printTrigger.click(function() {
-	websiteDesign = false;
-	print = true;
-	mobileApps = false;
-	all = false;
-	filterMaFolio();
-	console.log("printTrigger");
-});
-mobileAppsTrigger.click(function() {
-	websiteDesign = false;
-	print = false;
-	mobileApps = true;
-	all = false;
-	filterMaFolio();
-	console.log("mobileAppsTrigger");
-});
-allTrigger.click(function() {
-	websiteDesign = false;
-	print = false;
-	mobileApps = false;
-	all = true;
-	filterMaFolio();
-	console.log("allTrigger");
-});
-
-function aniHide(item) {
-	item.slideUp(2000);
-	item.detach();
+function filteritman(cat) {
+	$grid.isotope({ filter: '.category-' + cat });	
 }
-function aniShow(item) {
-	item.appendTo('.portfolio-grid');
-	item.slideDown(2000);
+function showall() {
+$grid.isotope({ filter: '*' });
 }
-
-function filterMaFolio() {
-	
-	 /*if (all) {
-	 	aniShow(allDOM);
-	} else {
-	 	
-
-allDOM.hide();
-	if (websiteDesign) {
-		aniShow(websiteDesignDOM);
-	} else {
-		aniHide(websiteDesignDOM);
-	}
-	 if (print) {
-	 	aniShow(printDOM);
-	} else {
-		aniHide(printDOM);
-	}
-	 if (mobileApps) {
-	 	aniShow(mobileAppsDOM);
-	} else {
-		aniHide(mobileAppsDOM);
-	//}
-	}
-	}*/
 </script>
 <?php get_footer(); ?>
-
