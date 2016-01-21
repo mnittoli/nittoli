6dd113d9cb58a68adbb48ecaebefd65302a19b14
mnittoli <nittoli1111@gmail.com>
2016-01-21 08:43:10 -0800

    Theme Upload Bugs

--- functions.php
+++ functions.php
@@ -167,70 +167,8 @@ add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
 
 ?>
 
-
 <?php
-// hook into the init action and call create_book_taxonomies when it fires
-add_action( 'init', 'create_book_taxonomies', 0 );
 
-// create two taxonomies, genres and writers for the post type "book"
-function create_book_taxonomies() {
-    // Add new taxonomy, make it hierarchical (like categories)
-    $labels = array(
-        'name'              => _x( 'Genres', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Genre', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Genres' ),
-        'all_items'         => __( 'All Genres' ),
-        'parent_item'       => __( 'Parent Genre' ),
-        'parent_item_colon' => __( 'Parent Genre:' ),
-        'edit_item'         => __( 'Edit Genre' ),
-        'update_item'       => __( 'Update Genre' ),
-        'add_new_item'      => __( 'Add New Genre' ),
-        'new_item_name'     => __( 'New Genre Name' ),
-        'menu_name'         => __( 'Genre' ),
-    );
-
-    $args = array(
-        'hierarchical'      => true,
-        'labels'            => $labels,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'genre' ),
-    );
-
-    register_taxonomy( 'genre', array( 'post' ), $args );
-
-    // Add new taxonomy, NOT hierarchical (like tags)
-    $labels = array(
-        'name'                       => _x( 'Writers', 'taxonomy general name' ),
-        'singular_name'              => _x( 'Writer', 'taxonomy singular name' ),
-        'search_items'               => __( 'Search Writers' ),
-        'popular_items'              => __( 'Popular Writers' ),
-        'all_items'                  => __( 'All Writers' ),
-        'parent_item'                => null,
-        'parent_item_colon'          => null,
-        'edit_item'                  => __( 'Edit Writer' ),
-        'update_item'                => __( 'Update Writer' ),
-        'add_new_item'               => __( 'Add New Writer' ),
-        'new_item_name'              => __( 'New Writer Name' ),
-        'separate_items_with_commas' => __( 'Separate writers with commas' ),
-        'add_or_remove_items'        => __( 'Add or remove writers' ),
-        'choose_from_most_used'      => __( 'Choose from the most used writers' ),
-        'not_found'                  => __( 'No writers found.' ),
-        'menu_name'                  => __( 'Writers' ),
-    );
-
-    $args = array(
-        'hierarchical'          => false,
-        'labels'                => $labels,
-        'show_ui'               => true,
-        'show_admin_column'     => true,
-        'update_count_callback' => '_update_post_term_count',
-        'query_var'             => true,
-        'rewrite'               => array( 'slug' => 'writer' ),
-    );
-
-    register_taxonomy( 'writer', 'post', $args );
 // create excerpt
 function new_excerpt_more( $more ) {
     return ' <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">' . __( 'Read More', 'your-text-domain' ) . '</a>';
@@ -241,7 +179,7 @@ function custom_excerpt_length( $length ) {
 						return 20;
 					}
 					add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
-}
+
 function filter_ptags_on_images($content){
    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
